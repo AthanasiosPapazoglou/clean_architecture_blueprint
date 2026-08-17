a616ac28683f888b8fad873f2eab71d1aa6d7e4e
Athanasios Papazoglou <athanasiospap95@gmail.com>
2026-08-17 18:57:46 +0200

    test github status

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-//test
+//test github status
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
